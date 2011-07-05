c3836600aed070d98c3898470141d1a2af97a585
Matt Westcott <matt@west.co.tt>
2011-07-05 18:44:51 +0200

    move Spectrum module into its own file

--- jsspeccy.js
+++ jsspeccy.js
@@ -1,35 +1,3 @@
-function Spectrum(opts) {
-	var self = {};
-	
-	var ui = opts.ui;
-	var keyboard = opts.keyboard;
-	
-	var memory = Memory();
-	
-	var display = Display({
-		ui: ui,
-		memory: memory
-	});
-	
-	var ioBus = IOBus({
-		keyboard: keyboard,
-		display: display,
-		memory: memory
-	});
-	
-	var processor = Z80({
-		memory: memory,
-		ioBus: ioBus,
-		display: display
-	});
-	
-	self.runFrame = function() {
-		processor.runFrame();
-	}
-	
-	return self;
-}
-
 function JSSpeccy(container) {
 	if (typeof(container) === 'string') {
 		container = document.getElementById(container);
