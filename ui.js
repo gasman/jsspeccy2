8c26292b7d747c9e73281f52299a6895e948a548
Matt Westcott <matt@west.co.tt>
2012-06-30 23:07:28 +0200

    Implement WOS API integration

--- ui.js
+++ ui.js
@@ -109,6 +109,101 @@ JSSpeccy.UI = function(opts) {
 		}
 	};
 
+	if (jQuery) {
+		(function ($) {
+			var wosSearchField = $('<input type="text">');
+			var wosSearchBtn = $('<input type="submit" value="Search title">');
+			var wosSearch = $('<form></form>');
+			var wosMatches = $('<select style="width: 250px" size="8"></select>');
+			var wosDownloads = $('<select style="width: 250px" size="8"></select>');
+			var wosOpen = $('<button>Open file</button>').attr('disabled', 'disabled');
+			$(openFilePanel).append(
+				'<h2>Search World Of Spectrum:</h2>', wosSearch, wosMatches, wosDownloads, wosOpen
+			);
+			wosSearch.append(wosSearchField, wosSearchBtn);
+			wosSearch.submit(function() {
+				var query = wosSearchField.val();
+				if (query !== '') {
+					$.getJSON('http://www.worldofspectrum.org/api/infoseek_search_json.cgi?callback=?',
+						{title: query},
+						function(results) {
+							wosMatches.empty();
+							wosDownloads.empty();
+							wosOpen.attr('disabled', 'disabled');
+							if (results.matches) {
+								for (var i = 0; i < results.matches.length; i++) {
+									var result = results.matches[i];
+									var optionText = result.title;
+									if (result.publisher) {
+										optionText += " (" + result.publisher + ")";
+									}
+									var option = $('<option></option>').text(optionText).attr('value', result.id);
+									wosMatches.append(option);
+								}
+								wosMatches.removeAttr('disabled');
+							} else {
+								wosMatches.append('<option>(no matches found)</option>');
+								wosMatches.attr('disabled', 'disabled');
+							}
+						}
+					);
+				}
+				return false;
+			});
+
+			wosMatches.change(function() {
+				wosDownloads.empty();
+				wosOpen.attr('disabled', 'disabled');
+				var id = $(this).val();
+				if (id) {
+					$.getJSON('http://www.worldofspectrum.org/api/infoseek_select_json.cgi?callback=?',
+						{id: id},
+						function(response) {
+							wosDownloads.empty();
+							if (response.downloads) {
+								for (var i = 0; i < response.downloads.length; i++) {
+									var download = response.downloads[i];
+									var optionText;
+									if (download.origin !== '') {
+										optionText = download.origin + " - " + download.type;
+									} else {
+										optionText = download.type;
+									}
+									var option = $('<option></option>').text(optionText).attr('value', download.link);
+									wosDownloads.append(option);
+								}
+								wosDownloads.removeAttr('disabled');
+							} else {
+								wosDownloads.append('<option>(no downloads available)</option>');
+								wosDownloads.attr('disabled', 'disabled');
+							}
+						}
+					);
+				}
+			});
+
+			wosDownloads.change(function() {
+				var url = $(this).val();
+				if (url) {
+					wosOpen.removeAttr('disabled');
+				} else {
+					wosOpen.attr('disabled', 'disabled');
+				}
+			});
+
+			function loadSelectedFile() {
+				var url = wosDownloads.val();
+				if (url) {
+					controller.loadFromUrl(url.replace('ftp://ftp.worldofspectrum.org/pub/sinclair/', 'http://wosproxy.zxdemo.org/unzip/'));
+					hidePanels();
+				}
+			}
+
+			wosOpen.click(loadSelectedFile);
+			wosDownloads.dblclick(loadSelectedFile);
+		})(jQuery);
+	}
+
 	function showPanel(requestedPanel) {
 		for (var i = 0; i < panels.length; i++) {
 			panels[i].style.display = (panels[i] == requestedPanel ? 'block' : 'none');
