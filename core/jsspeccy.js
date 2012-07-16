46411dae76592bb6620c1b9a7364a61ce4b6f880
Matt Westcott <matt@west.co.tt>
2012-07-16 15:45:46 +0200

    version stamping + link to embedding instructions

--- core/jsspeccy.js
+++ core/jsspeccy.js
@@ -1,3 +1,19 @@
+/**
+ * @license JSSpeccy v2.1 - http://jsspeccy.zxdemo.org/
+ * Copyright 2012 Matt Westcott <matt@west.co.tt>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 if (!window.DataView) window.DataView = jDataView;
 
 function JSSpeccy(container, opts) {
